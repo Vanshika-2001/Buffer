c2c938521eadb18c64128c33d6b6c35ea44d2bf5
Shreya Nair <shreyanair62@gmail.com>
2021-04-06 21:20:08 +0530

    Clothing class header added.

---
+++ Clothing Store/Clothing.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+class Clothing {
+	public:
+	 int ID;
+	 string name;
+	 string type;
+	 string brand;
+	 int price;
+	 void display();
+};
+// This is Clothing class declaration.

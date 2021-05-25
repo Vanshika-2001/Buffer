dce2d325f4b577d05b91593eb71ad8333435849b
fatmab2809 <fatmab2809@gmail.com>
2021-05-25 18:36:10 +0530

    Resolved searching a product issue and made some changes in the main.cpp file

--- Clothing Store/customer.cpp
+++ Clothing Store/customer.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include<iomanip>
 #include <algorithm>
+#include <sstream>
+#include <vector>
 #include "Clothing.h"
 
 
@@ -60,9 +62,9 @@ void LinkedList :: sort1(int lower, int upper)
     
     
 }
-void LinkedList :: filter(){
+int LinkedList :: filter(){
     
-    int choice;
+    int choice, flag=1;
     cout<<"\t \t \t \t \t Select a price ranges from the options given below : "<<endl;
     cout<<"\t \t \t \t \t 1.Rs.500 to Rs.1000"<<endl<<"\t \t \t \t \t 2.Rs.1000 to Rs.1500"<<endl<<"\t \t \t \t \t 3.Rs.1500 to Rs.2000"<<endl;
     cout<<"\t \t \t \t \t ";
@@ -77,24 +79,32 @@ void LinkedList :: filter(){
 		case 3:sort1(1501,2000);
 		break;
 		default: cout<<"\t \t \t \t \t Invalid entry! \n"<<endl;
-        
+				flag = 0;
       }
-
+	return flag;
     
 }
-void LinkedList :: search(){
-  string Input, brand2, name2, type2;
+int LinkedList :: search(){
+  string Input, brand2, name2, type2, tmp;
+  int flag=0;
   cout<< "\t \t \t \t \t Search for something... \n"<<endl;
   cout<<"\t \t \t \t \t ";
-  cin>> Input;
+  cin.ignore();
+  getline(cin, Input);
   cout<<"\n";
   transform(Input.begin(), Input.end(), Input.begin(), ::tolower );
   temp = head;
+  
+  stringstream s(Input);
+  vector<string> words;  //vector to store words of string Input
+  while( s >> tmp ){     //splits string into tmp words
+	  words.push_back(tmp);  //pushes the tmp words into vector words
+  }
+	  
   if(head == NULL){
       cout<< "\t \t \t \t \t List is empty! \n"<<endl;
   }
   else{
-      int flag = 0;
       int counter =1;
       while(temp!= NULL){
           name2 = temp -> name;
@@ -103,24 +113,28 @@ void LinkedList :: search(){
           transform(name2.begin(), name2.end(), name2.begin(), ::tolower );
           transform(brand2.begin(), brand2.end(), brand2.begin(), ::tolower );
           transform(type2.begin(), type2.end(), type2.begin(), ::tolower );
-
-          if(name2.find(Input) != string::npos || brand2.find(Input) != string::npos || type2.find(Input) != string::npos){
-               if (counter==1){
-			 cout<<"\t -------------------------------------------------------------------------------------------------"<<endl;
-	         cout<<"\t "<<setw(10)<<"Product Id"<< setw(35) <<"Name"<<setw(20)<<"Type" <<setw(20)<<"Brand"<<setw(10)<< "Price"<<endl;
-			 cout<<"\t -------------------------------------------------------------------------------------------------"<<endl;
-	         counter++;
-               }
-             cout <<"\t "<<setw(10)<< temp->ID <<setw(35)<< temp->name<<setw(20)<<temp->type<<setw(20)<<temp->brand<<setw(10)<<temp->price<<endl;
-             flag = 1;
-
-          }
+		  
+			for( int i=0; i<words.size(); ++i){   //iterating through each word in vector words
+				if(name2.find(words[i]) != string::npos || brand2.find(words[i]) != string::npos || type2.find(words[i]) != string::npos){ 
+				   if (counter==1){
+					 cout<<"\t -------------------------------------------------------------------------------------------------"<<endl;
+					 cout<<"\t "<<setw(10)<<"Product Id"<< setw(35) <<"Name"<<setw(20)<<"Type" <<setw(20)<<"Brand"<<setw(10)<< "Price"<<endl;
+					 cout<<"\t -------------------------------------------------------------------------------------------------"<<endl;
+					 counter++;
+					   }
+					 cout <<"\t "<<setw(10)<< temp->ID <<setw(35)<< temp->name<<setw(20)<<temp->type<<setw(20)<<temp->brand<<setw(10)<<temp->price<<endl;
+					 flag = 1;
+				  }
+			}
+			 
+         
           temp = temp -> next;
      }
       if(flag == 0){
           cout <<"\t \t \t \t \t Product not available! \n "<<endl;
       }
   }
+  return flag;
 
 }
 void LinkedList::buysearch(LinkedList* cart,int &total)
